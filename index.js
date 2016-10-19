8025a6c676ad3501a209d7292595ba0f3e0e4399
Graf, Greg <greg.graf@viacom.com>
2016-10-19 07:33:33 -0400

    added code to generate and save html, as well as initial pass at styling

--- index.js
+++ index.js
@@ -1,29 +1,189 @@
+var fs = require( "fs" );
+var os = require('os')
+var path = require('path')
 
 var SpecAsHTMLReporter = function( baseReporterDecorator, formatError, config ) {
 
 	baseReporterDecorator(this);
 
-	this.specSuccess = function(a, b) {
-		if ( b && b.description ) {
-			return '<li class="success">' + b.description + '</li>';
-		}
+	var self = this;
+
+	self.suites = {};
+
+	var Suite = function( description ) {
+
+		var items = [];
+
+		var suite = {
+
+			add: function( result, success ) {
+
+				if ( result.render ) {
+					items.push( result )
+				} else {
+					items.push( makeItem ( result, success ) )
+				}
+			},
+
+			render: function( margin ) {
+
+				margin = margin || "";
+				var indent = margin + "  ";
+
+				var output = [
+					margin + '<li class="suite">' + description,
+					indent + '<ul>'
+				];
+
+
+				items.forEach( function( item ) {
+
+					if ( item.render ) {
+
+						output = output.concat( item.render( indent + "  " ) );
+
+					} else {
+
+						output.push( indent + '  ' + item );
+
+					}
+				});
+
+				output = output.concat( [
+					indent + '</ul>',
+					margin + '</li>'
+				] );
+
+				return output;
+			}
+		};
+		return suite;
 	};
 
-	this.specFailure = function(a, b) {
-		if ( b && b.description ) {
-			return '<li class="failure">' + b.description + '</li>';
+	var baseSuite = Suite( "base" );
+
+	var checkSuites = function( suiteLabels ) {
+
+		var suites = self.suites;
+
+		if ( suiteLabels.forEach ) {
+
+			var last = suiteLabels.length - 1;
+			var parentSuite = baseSuite;
+
+			suiteLabels.forEach( function( label ) {
+
+				var suite = suites[ label ];
+
+				if ( !suite ) {
+
+					suite = Suite( label );
+					suites[ label ] = suite;
+					parentSuite.add( suite );
+
+				}
+
+				parentSuite = suite;
+
+			});
+
+			return suites[ suiteLabels[ last ] ]
+
 		}
+
 	};
 
-	this.specSkipped = function(a, b) {
-		if ( b && b.description ) {
-			return '<li class="skipped">' + b.description + '</li>';
+
+	var makeItem = function( results, success ) {
+
+		var passed = success? "success": "failure";
+
+		var description = results && results.description;
+
+		if ( description ) {
+			return '<li class="' + passed + '">' + description + '</li>';
+		} else {
+			return '';
 		}
+
 	};
+
+	var add = function( result, status) {
+
+		if ( result && result.suite ) {
+			var activeSuite = checkSuites( result.suite )
+			activeSuite.add( result, status );
+		}
+
+	}
+
+	this.getStyles = function () {
+
+		var output = [
+			'body {',
+			'  font-family: Helvetica, Arial, sans-serif;',
+			'  font-size: .9em;',
+			'  line-height: 1.5;',
+			'}',
+			'.suite ul {list-style-type: none; margin-top: .4em }',
+			'.suite li { margin-bottom: .8em; }',
+			'.suite { margin-top: 1em }',
+			'.success {color: green }',
+			'.success:before { content: "✓ " }',
+			'.failure {color: red }',
+			'.failure:before { content: "✗ " }'
+		]
+		return output.join("\n");
+	}
+
+	this.getResultsMarkup = function () {
+		return baseSuite.render().join('\n') + '\n'
+	}
+
+	this.render = function () {
+
+		var markup = [
+			'<!DOCTYPE html>',
+			'<html lang="en">',
+			'<head>',
+			'  <meta charset="utf-8">',
+			'  <meta name="viewport" content="width=device-width">',
+			'  <title>Spec for</title>',
+			'  <style>',
+			self.getStyles(),
+			'  </style>',
+			'</head>',
+			'<body>',
+			self.getResultsMarkup(),
+			'</body>',
+			'</html>'
+		]
+
+		return markup.join('\n') + '\n';
+	}
+
+	this.specSuccess = function( browser, results ) {
+		add( results, true );
+	};
+
+	this.specFailure = function( browser, results ) {
+		add( results );
+	};
+
+    this.onRunComplete = function( ) {
+
+    	console.log("config.basePath", config.basePath + "/spec.html", self.render() )
+
+		fs.writeFile( config.basePath + "/spec.html", self.render(), function( err) {
+			console.log(err)
+		} );
+	};
+
+
 };
 
-SpecAsHTMLReporter.$inject = ['baseReporterDecorator', 'formatError', 'config']
+SpecAsHTMLReporter.$inject = [ 'baseReporterDecorator', 'formatError', 'config' ]
 
 module.exports = {
-  'reporter:spec-as-html': ['type', SpecAsHTMLReporter]
+  'reporter:spec-as-html': [ 'type', SpecAsHTMLReporter ]
 };
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -7,7 +7,8 @@ baseReporterDecorator { adapters: [ [Function: bound ] ],
   writeCommonMsg: [Function],
   onBrowserError: [Function],
   onBrowserLog: [Function],
-  onSpecComplete: [Function],
+  on
+  Complete: [Function],
   specSkipped: [Function],
   specSuccess: [Function],
   specFailure: [Function],
@@ -37,44 +38,286 @@ baseReporterDecorator { adapters: [ [Function: bound ] ],
 
 */
 
+var fs = require( "fs" );
+
 var SpecAsHTMLReporter = require('../index.js')['reporter:spec-as-html'];
 
 describe( "karma-spec-as-html-reporter", function() {
 
 	var reporter;
 
-	var baseReporterDecorator = function( instance ) {
-
-	};
-
+	var baseReporterDecorator = function( instance ) {};
 
 	beforeEach( function() {
 
-		reporter = new SpecAsHTMLReporter[1]( baseReporterDecorator );
+		reporter = new SpecAsHTMLReporter[1]( baseReporterDecorator, {}, { basePath: "/project/"} );
+		spyOn(fs, "writeFile")
 
 	} );
 
 
-	it( "specSuccess should render a successful test as a list item with class of 'success'", function() {
+	it( "should not add a suite more than once", function() {
 
-		expect( reporter.specSuccess({}, { 'description': 'should do the thing'}) )
-			.toBe('<li class="success">should do the thing</li>');
+		var sampleResults = [
+			{
+				'description': "should do the first thing",
+				'suite': [ "suite1" ]
+			},
+			{
+				'description': "should do the second thing",
+				'suite': [ "suite1", "suite2" ]
+			},
+			{
+				'description': "should do the third thing",
+				'suite': [ "suite1", "suite2" ]
+			},
+			{
+				'description': "should do the fourth thing",
+				'suite': [ "suite1", "suite2", "suite3" ]
+			},
+			{
+				'description': "should do the fifth thing",
+				'suite': [ "suite1" ]
+			}
+		];
+
+		sampleResults.forEach( function( suite ) {
+			reporter.specSuccess( {}, suite );
+		} );
+
+		expect( Object.keys( reporter.suites ).length ).toBe( 3 );
+
+	} );
+
+	it( "should properly render a suite with a single successful result ", function() {
+
+		reporter.specSuccess(
+			{},
+			{
+				'description': "should do the first thing",
+				'suite': [ "suite1" ]
+			}
+		);
+
+		var expectedResult = [
+			'<li class="suite">base',
+			'  <ul>',
+			'    <li class="suite">suite1',
+			'      <ul>',
+			'        <li class="success">should do the first thing</li>',
+			'      </ul>',
+			'    </li>',
+			'  </ul>',
+			'</li>',
+			''
+		].join( '\n' );
+
+		var result = reporter.getResultsMarkup()
+		expect( result ).toBe( expectedResult );
+		expect( result ).toMatch( /<li class="success">/ );
 
 	} );
 
 
-	it( "specFailure should render a failed test as a list item with class of 'failure'", function() {
+	it( "should not render a suite with a single successful result lacking description ", function() {
 
-		expect( reporter.specFailure({}, { 'description': 'should do the thing'}) )
-			.toBe('<li class="failure">should do the thing</li>');
+		reporter.specSuccess(
+			{},
+			{
+				'suite': [ "suite1" ]
+			}
+		);
+
+		var expectedResult = [
+			'<li class="suite">base',
+			'  <ul>',
+			'    <li class="suite">suite1',
+			'      <ul>',
+			'        ',
+			'      </ul>',
+			'    </li>',
+			'  </ul>',
+			'</li>',
+			''
+		].join( '\n' );
+
+		var result = reporter.getResultsMarkup()
+		expect( result ).toBe( expectedResult );
+
+	} );
+
+	it( "should properly render a suite with a single failure result ", function() {
+
+		reporter.specFailure( {},
+			{
+				'description': "should do the first thing",
+				'suite': [ "suite1" ]
+			}
+		);
+
+		var expectedResult = [
+			'<li class="suite">base',
+			'  <ul>',
+			'    <li class="suite">suite1',
+			'      <ul>',
+			'        <li class="failure">should do the first thing</li>',
+			'      </ul>',
+			'    </li>',
+			'  </ul>',
+			'</li>',
+			''
+		].join( '\n' );
+
+		var result = reporter.getResultsMarkup()
+		expect( result ).toBe( expectedResult );
+		expect( result ).toMatch( /<li class="failure">/ );
+
+	} );
+
+	it( "should properly render two suites", function() {
+
+		reporter.specSuccess( {},
+			{
+				'description': "should do the first thing",
+				'suite': [ "suite1" ]
+			}
+		);
+
+		reporter.specSuccess( {},
+			{
+				'description': "should do the second thing",
+				'suite': [ "suite2" ]
+			}
+		);
+
+		var expectedResult = [
+			'<li class="suite">base',
+			'  <ul>',
+			'    <li class="suite">suite1',
+			'      <ul>',
+			'        <li class="success">should do the first thing</li>',
+			'      </ul>',
+			'    </li>',
+			'    <li class="suite">suite2',
+			'      <ul>',
+			'        <li class="success">should do the second thing</li>',
+			'      </ul>',
+			'    </li>',
+			'  </ul>',
+			'</li>',
+			''
+		].join( '\n' );
+
+		var result = reporter.getResultsMarkup()
+		expect( result ).toBe( expectedResult );
 
 	} );
 
 
-	it( "specSkipped should render a successful test as a list item with class of 'skipped'", function() {
+	it( "should properly render a suite with nested suites ", function() {
 
-		expect( reporter.specSkipped({}, { 'description': 'should do the thing'}) )
-			.toBe('<li class="skipped">should do the thing</li>');
+		var sampleResults = [
+			{
+				'description': "should do the first thing",
+				'suite': [ "suite1" ]
+			},
+			{
+				'description': "should do the second thing",
+				'suite': [ "suite1", "suite2" ]
+			},
+			{
+				'description': "should do the third thing",
+				'suite': [ "suite1", "suite2" ]
+			},
+			{
+				'description': "should do the fourth thing",
+				'suite': [ "suite1", "suite2", "suite3" ]
+			},
+			{
+				'description': "should do the fifth thing",
+				'suite': [ "suite1" ]
+			}
+		];
+
+		reporter.specSuccess( {}, sampleResults[0] );
+		reporter.specFailure( {}, sampleResults[1] );
+		reporter.specSuccess( {}, sampleResults[2] );
+		reporter.specSuccess( {}, sampleResults[3] );
+		reporter.specSuccess( {}, sampleResults[4] );
+
+
+		var expectedResult = [
+			'<li class="suite">base',
+			'  <ul>',
+			'    <li class="suite">suite1',
+			'      <ul>',
+			'        <li class="success">should do the first thing</li>',
+			'        <li class="suite">suite2',
+			'          <ul>',
+			'            <li class="failure">should do the second thing</li>',
+			'            <li class="success">should do the third thing</li>',
+			'            <li class="suite">suite3',
+			'              <ul>',
+			'                <li class="success">should do the fourth thing</li>',
+			'              </ul>',
+			'            </li>',
+			'          </ul>',
+			'        </li>',
+			'        <li class="success">should do the fifth thing</li>',
+			'      </ul>',
+			'    </li>',
+			'  </ul>',
+			'</li>',
+			''
+		].join( '\n' );
+
+		var result = reporter.getResultsMarkup()
+
+		expect( result ).toBe( expectedResult );
+
+
+	} );
+
+	describe( "render of html page", function() {
+
+		it( "should have proper base markup ", function() {
+
+			spyOn( reporter, "getStyles");
+			spyOn( reporter, "getResultsMarkup");
+
+			var expectedResult = [
+				'<!DOCTYPE html>',
+				'<html lang="en">',
+				'<head>',
+				'  <meta charset="utf-8">',
+				'  <meta name="viewport" content="width=device-width">',
+				'  <title>Spec for</title>',
+				'  <style>',
+				'',
+				'  </style>',
+				'</head>',
+				'<body>',
+				'',
+				'</body>',
+				'</html>',
+				''
+			].join( '\n' );
+
+			var result = reporter.render()
+			expect( result ).toBe( expectedResult );
+
+		} );
+
+	} );
+
+	describe( "saving to file", function() {
+
+		it( "should call writeFile ", function() {
+
+			reporter.onRunComplete();
+			expect( fs.writeFile ).toHaveBeenCalledWith(  );
+
+		} );
 
 	} );
 
